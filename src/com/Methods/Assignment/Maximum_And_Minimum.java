de7f29b6e7912ed2da33f5f3384370b7a5ca01a9
Pawan <paletipawankumar@gmail.com>
2022-08-01 17:00:02 +0530

    Adding another way of solution to the question

--- src/com/Methods/Assignment/Maximum_And_Minimum.java
+++ src/com/Methods/Assignment/Maximum_And_Minimum.java
@@ -9,10 +9,38 @@ public class Maximum_And_Minimum {
         int num1 = sc.nextInt();
         int num2 = sc.nextInt();
         int num3 = sc.nextInt();
-        System.out.println("the maximum number entered  is " + maximum(num1,num2,num3));
-        System.out.println("the minimum number entered is " + minimum(num1,num2,num3));
+        System.out.println("The greatest number entered is " + greatest(num1,num2,num3));
+        System.out.println("The smallest number entered is " + smallest(num1,num2,num3));
+//        System.out.println("the maximum number entered  is " + maximum(num1,num2,num3));
+//        System.out.println("the minimum number entered is " + minimum(num1,num2,num3));
     }
 
+//    this method is used for finding the greatest number
+    static int greatest(int a , int b ,int c){
+        int max = a;
+        if (b>max){
+            max = b;
+        }
+        if (c>max){
+            max = c;
+        }
+        return max;
+    }
+
+//    this method is used for finding the smallest number
+    static int smallest(int a , int b , int c){
+        int min = a;
+        if (b < min){
+            min = b;
+        }
+        if (c<min){
+            min = c;
+        }
+        return min;
+    }
+
+//    this also another way to solve the question
+    /*
     static int maximum(int a , int b , int c){
         if (a>b && a>c){
             return a;
@@ -32,4 +60,6 @@ public class Maximum_And_Minimum {
             return c;
         }
     }
+
+     */
 }
