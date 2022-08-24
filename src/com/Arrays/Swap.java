67dcef90df73e3f5df947d0a8c0f8ab55bbe3458
Pawan <paletipawankumar@gmail.com>
2022-08-24 15:38:53 +0530

    adding program of max value in the repo

--- src/com/Arrays/Swap.java
+++ src/com/Arrays/Swap.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Swap {
     public static void main(String[] args) {
         int[] arr = {1 , 54 , 6 , 7 , 10};
-        swap(arr , 2 , 4);
+        swap(arr , 0 , 4);
         System.out.println(Arrays.toString(arr));
 
     }
