16027f423c4451be7f4d4fbb1f1bbd0dbf337e5b
Pawan <paletipawankumar@gmail.com>
2022-08-24 16:14:56 +0530

    Added Program of reversing a array

--- src/com/Arrays/Max_In_Range.java
+++ src/com/Arrays/Max_In_Range.java
@@ -6,13 +6,13 @@ public class Max_In_Range {
     public static void main(String[] args) {
 
         int[] arr = { 1 , 5 , 4 , 8 , 7 , 9};
-        int ans = range(arr,1,4);
+        int ans = range(arr,2,5);
         System.out.println(ans);
 
     }
     static int range(int[] arr , int start , int end){
         int max = arr[start];
-        for (int i = start; i <end ; i++) {
+        for (int i = start; i <=end ; i++) {
             if (arr[i]>max){
                 max = arr[i];
             }
