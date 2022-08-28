d4a5a73c9369970c2d79f1eca45f72df2bb1736f
Pawan <paletipawankumar@gmail.com>
2022-08-28 12:52:23 +0530

    modifing the linear search

--- src/com/Linear_Search/Search_InString.java
+++ src/com/Linear_Search/Search_InString.java
@@ -1,12 +1,30 @@
 package com.Linear_Search;
 
+import java.util.Arrays;
+
 public class Search_InString {
     public static void main(String[] args) {
         String name = "Pawan";
         char target = 'a';
         System.out.println(linearSearch(name,target));
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(linearSearch2(name,target));
 
     }
+
+//    iterating over array using for each loop
+    static boolean linearSearch2(String str , char target){
+        if (str.length()==0){
+            return false;
+        }
+        for (char ch:str.toCharArray()){
+            if (ch==target){
+                return true;
+            }
+        }
+        return false;
+    }
+
     static boolean linearSearch(String str , char target){
         if (str.length()==0){
             return false;
