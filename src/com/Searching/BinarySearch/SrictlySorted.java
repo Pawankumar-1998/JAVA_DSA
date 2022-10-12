963648c659967a839daa567d784ed48d18244bd4
Pawan <paletipawankumar@gmail.com>
2022-10-12 21:53:33 +0530

    Adding strictly binarySearch

---
+++ src/com/Searching/BinarySearch/SrictlySorted.java
@@ -0,0 +1,10 @@
+package com.Searching.BinarySearch;
+
+public class SrictlySorted {
+    public static void main(String[] args) {
+
+    }
+    static int[] binarySearch(int[][] matrix , int target){
+        int rows = matrix.length;
+    }
+}
