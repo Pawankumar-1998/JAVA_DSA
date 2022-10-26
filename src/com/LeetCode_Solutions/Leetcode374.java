4e8254b2f5e52e677ea726232e0a2189ed2465dd
Pawan <paletipawankumar@gmail.com>
2022-10-26 19:00:14 +0530

    Adding leetcode 374 to the repo

---
+++ src/com/LeetCode_Solutions/Leetcode374.java
@@ -0,0 +1,36 @@
+package com.LeetCode_Solutions;
+
+public class Leetcode374 {
+    public static void main(String[] args) {
+        int n = 10;
+        int pick = 18;
+        System.out.println(guessTheNumber(n,pick));
+    }
+    static int guessTheNumber(int n , int pick){
+        int start = 0;
+        int end = n;
+        while (start<=end){
+            int mid = start + (end - start)/2;
+            if (guess(mid,pick)==0){
+                return mid;
+            }
+            if (guess(mid,pick)==1){
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    static int guess(int guess , int pick){
+        if (guess==pick){
+            return 0;
+        }
+        if (guess<pick){
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+}
