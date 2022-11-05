693a4a8389325f0a30033e8539efdd62ee60a0d9
Pawan <paletipawankumar@gmail.com>
2022-11-05 17:24:13 +0530

    Adding cyclic sort to the repo

---
+++ src/com/Cyclic_Sort/code.java
@@ -0,0 +1,29 @@
+package com.Cyclic_Sort;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class code {
+    public static void main(String[] args) {
+        int[] arr = {2,3,1,4,6,7};
+        cyclicSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void cyclicSort(int[] arr){
+        int i = 0;
+        while (i< arr.length){
+            int correct = arr[i] - 1;
+            if (arr[i]!=arr[correct]){
+                swap(arr,i,correct);
+            } else {
+                i++;
+            }
+        }
+     }
+
+     static void swap(int[] arr , int first , int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+     }
+}
