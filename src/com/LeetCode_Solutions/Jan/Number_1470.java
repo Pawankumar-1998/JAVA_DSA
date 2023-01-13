d688686cb84111cb3fee8d8b28e8a2eedaa65d9f
Pawan <paletipawankumar@gmail.com>
2023-01-13 21:10:39 +0530

    changed value from 3 to 4

--- src/com/LeetCode_Solutions/Jan/Number_1470.java
+++ src/com/LeetCode_Solutions/Jan/Number_1470.java
@@ -6,10 +6,10 @@ public class Number_1470 {
     public static void main(String[] args) {
         /*
     Input: nums = [2,5,1,3,4,7], n = 3
-    Output: [2,3,5,4,1,7] 
+    Output: [2,3,5,4,1,7]
      */
         int[] nums = {2,5,1,3,4,7};
-        int n =3;
+        int n =4;
         System.out.println(Arrays.toString(shuffleArray(nums,n)));
 
     }
