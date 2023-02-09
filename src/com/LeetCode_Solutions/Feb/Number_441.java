3cbe2a82a8bf002a3a97a2d2a63d19d76568252e
Pawan <paletipawankumar@gmail.com>
2023-02-09 10:54:27 +0530

    Adding feb folder and leetcode number 441

---
+++ src/com/LeetCode_Solutions/Feb/Number_441.java
@@ -0,0 +1,22 @@
+package com.LeetCode_Solutions.Feb;
+
+public class Number_441 {
+    public static void main(String[] args) {
+        /*
+        Input :- 5
+        Output :- 2
+         */
+        int a = 5;
+//        arrangingCoins(arrangingCoins(a));
+        System.out.println(arrangingCoins(a));
+    }
+
+    static int arrangingCoins(int num){
+        int count = 0;
+        for (int i = 1; num >=i ; i++) {
+            count++;
+            num=num-i;
+        }
+        return count;
+    }
+}
