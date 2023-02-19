04a3c7794c73dc0ab379652766c5b4c379bd855b
Pawan <paletipawankumar@gmail.com>
2023-02-19 12:39:37 +0530

    Adding OOPS folder to the repo

---
+++ src/com/OOPS/Main.java
@@ -0,0 +1,50 @@
+package com.OOPS;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*
+        below object is created of the class and new keyword is used to dynamically allocate the
+        memory reference variables are in stack memory and objects are in the heap memory
+         */
+    Student pawan = new Student(25, "kumar",96.5f);
+//        pawan.rollNo; accessing the data member of class using dot operator through object
+//        System.out.println(pawan.rollNo);
+//        System.out.println(pawan.name);
+//        System.out.println(pawan.marks);
+
+//        using parameterized constructor
+        System.out.println(pawan.rollNo);
+        System.out.println(pawan.name);
+        System.out.println(pawan.marks);
+
+//        creating a new object
+//        Student rahul = new Student();
+//        System.out.println(rahul.rollNo);
+//        System.out.println(rahul.name);
+//        System.out.println(rahul.marks);
+
+        Student random = new Student();
+        System.out.println(random.name);
+
+    }
+}
+
+// creating a class .
+//class is a collection of data members and member functions
+class Student{
+    int rollNo; //  these are data members
+    String name;
+    float marks;
+
+    Student(){
+        this(3,"suman",65.4f);
+    }
+
+    Student(int  rollNo, String name , float marks){
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+}
+
