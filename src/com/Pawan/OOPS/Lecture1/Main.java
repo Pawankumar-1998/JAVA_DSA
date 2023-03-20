856a39729621578592c7402c48a35aa350c11b4d
Pawan <paletipawankumar@gmail.com>
2023-03-20 21:31:35 +0530

    rearranging the code with proper comments

--- src/com/Pawan/OOPS/Lecture1/Main.java
+++ src/com/Pawan/OOPS/Lecture1/Main.java
@@ -3,50 +3,28 @@ package com.Pawan.OOPS.Lecture1;
 public class Main {
     public static void main(String[] args) {
 
-        /*
-        below object is created of the class and new keyword is used to dynamically allocate the
-        memory reference variables are in stack memory and objects are in the heap memory
+//        creating a instance of the class (i.e object )
+        Student pawan = new Student();
+        /* Description of the above line
+        1. pawan is the reference variable which is located in stack memory but will be pointing
+           to the object in the heap memory
+        2. the "new" keyword is used to dynamically allocate the memory for the object
+        3. Student() is a default constructor called when the object is created
          */
-    Student pawan = new Student(25, "kumar",96.5f);
-//        pawan.rollNo; accessing the data member of class using dot operator through object
-//        System.out.println(pawan.rollNo);
-//        System.out.println(pawan.name);
-//        System.out.println(pawan.marks);
 
-//        using parameterized constructor
-        System.out.println(pawan.rollNo);
+        pawan.roll = 23;
+//        pawan.name = "P pawan Kumar";
+        pawan.percentage = 93.5f;
+
+        System.out.println(pawan.roll);
         System.out.println(pawan.name);
-        System.out.println(pawan.marks);
 
-//        creating a new object
-//        Student rahul = new Student();
-//        System.out.println(rahul.rollNo);
-//        System.out.println(rahul.name);
-//        System.out.println(rahul.marks);
-
-        Student pawan1 = new Student(20,"pawan",85.0f);
-//        System.out.println(random.name);
 
     }
 }
-
-// creating a class .
-//class is a collection of data members and member functions
-class Student{
-    int rollNo; //  these are data members
-    String name;
-    float marks;
-
-//    default constructor
-//    Student(){
-////        calling a constructor in a constructor using this keyword
-//        this(3,"suman",65.4f);
-//    }
-
-    Student(int  roll, String naam , float mark){
-        this.rollNo = roll;
-        this.name = naam;
-        this.marks = mark;
-    }
-}
-
+// creating a class
+ class Student{
+    int roll;
+    String name = "Pawan";
+    float percentage;  // these are instance variable
+ }
