c94f64c53950377470c2df7c112cf383446cb8cf
Pawan <paletipawankumar@gmail.com>
2023-05-02 20:28:11 +0530

    adding string operation code to the repo

---
+++ src/com/Pawan/Strings/Operations.java
@@ -0,0 +1,10 @@
+package com.Pawan.Strings;
+
+public class Operations {
+    public static void main(String[] args) {
+        System.out.println('a' + 'b');
+        System.out.println("a" + "b");
+        System.out.println('a' + 3);
+        System.out.println((char)('a' + 3));
+    }
+}
