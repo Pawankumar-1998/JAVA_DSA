cb5f0cbcb9b9e3469562b0316d0a757d0a8f8e69
Pawan <paletipawankumar@gmail.com>
2023-05-03 15:42:43 +0530

    Adding string to the folder

---
+++ src/com/Pawan/Strings/Test.java
@@ -0,0 +1,12 @@
+package com.Pawan.Strings;
+
+public class Test {
+    public static void main(String[] args) {
+        String name = "pawan";
+        System.out.println(name.charAt(2));
+        int n = name.length();
+        System.out.println(n);
+        boolean f = name.contains("q");
+        System.out.println(f);
+    }
+}
