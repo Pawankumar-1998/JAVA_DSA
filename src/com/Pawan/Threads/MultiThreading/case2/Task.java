8d02ab7b4771b681efbfe383969e929193e8e7a3
Pawan <paletipawankumar@gmail.com>
2023-06-04 13:02:59 +0530

    modifing the code in multi threading

--- src/com/Pawan/Threads/MultiThreading/case2/Task.java
+++ src/com/Pawan/Threads/MultiThreading/case2/Task.java
@@ -3,13 +3,20 @@ package com.Pawan.Threads.MultiThreading.case2;
 import java.lang.Thread;
 public class Task extends Thread {
     public void run(){
+
         System.out.println("book the ticket ");
     }
 
     public static void main(String[] args) {
+//        Task obj = new Task();
         Task obj1 = new Task();
         Task obj2 = new Task();
         obj1.start();
         obj2.start();
+
+//        Thread t1 = new Thread();
+//        Thread t2 = new Thread();
+//        t1.start();
+//        t2.start();
     }
 }
