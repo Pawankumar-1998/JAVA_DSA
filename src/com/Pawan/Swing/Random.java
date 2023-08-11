fb9d46624b2f1733e30c7e6cc6b5a5f56c37b1ba
Pawan <paletipawankumar@gmail.com>
2023-08-11 23:16:56 +0530

    adding the swing folder

---
+++ src/com/Pawan/Swing/Random.java
@@ -0,0 +1,11 @@
+package com.Pawan.Swing;
+
+import com.Pawan.OOPS.Lecture4.Parent;
+public class Random {
+        int number;
+    public static void main(String[] args) {
+        Random random = new Random();
+
+//        random.number = random.num;
+    }
+}
---
+++ src/com/Pawan/Swing/SubDif.java
@@ -0,0 +1,17 @@
+package com.Pawan.Swing;
+
+import com.Pawan.OOPS.Lecture4.Parent;
+
+public class SubDif extends Parent {
+
+    public SubDif(int num, String name) {
+        super(num, name);
+    }
+
+    public static void main(String[] args) {
+        SubDif obj = new SubDif(10,"pawan");
+
+
+
+    }
+}
