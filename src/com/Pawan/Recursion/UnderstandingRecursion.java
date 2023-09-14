2d82fa1cfb9f83ff3b132f7c1d2543f56f3ceeda
Pawan <paletipawankumar@gmail.com>
2023-09-15 00:09:36 +0530

    Adding the recursion folder

---
+++ src/com/Pawan/Recursion/UnderstandingRecursion.java
@@ -0,0 +1,34 @@
+package com.Pawan.Recursion;
+
+public class UnderstandingRecursion {
+    public static void main(String[] args) {
+        message();
+    }
+
+    static void message(){
+        System.out.println("Hii pawan");
+        message2();
+    }
+
+    static void message2(){
+        System.out.println("Hii pawan");
+        message3();
+    }
+
+    static void message3(){
+        System.out.println("Hii pawan");
+        message4();
+    }
+
+    static void message4(){
+        System.out.println("Hii pawan");
+        message5();
+    }
+
+    static void message5(){
+        System.out.println("Hii pawan");
+    }
+
+
+
+}
