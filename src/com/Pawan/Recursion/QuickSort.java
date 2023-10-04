91c95b2fea7a8ce007038639c04cb35c4d4cfee9
Pawan <paletipawankumar@gmail.com>
2023-10-04 22:43:47 +0530

    adding the code of quick sort

---
+++ src/com/Pawan/Recursion/QuickSort.java
@@ -0,0 +1,55 @@
+package com.Pawan.Recursion;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+            int[] arr = { 5 , 4, 3 , 2, 0};
+            f(arr,0,arr.length-1);
+st        System.out.println(Arrays.toString(arr));
+    }
+
+    static void f(int[] arr , int low , int high ) {
+        if (low >= high) {
+            return;
+        }
+//            pivot index
+
+
+        int start = low;
+        int end = high;
+        int mid = start + (end-start) / 2;
+        int pivotNumber = arr[mid];
+
+        while (start <= end) {
+
+//            keep going if the start is less than pivot
+            while (arr[start] < pivotNumber) {
+                start++;
+            }
+
+//            keep going if the end is greater than the pivot
+            while (arr[end] > pivotNumber) {
+                end--;
+            }
+
+            if (start<=end) {
+//            swap
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+
+        }
+//            send the left array
+        f(arr, low, end);
+
+//            send the right array
+        f(arr, start , high);
+    }
+
+    }
+
+
