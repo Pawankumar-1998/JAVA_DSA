d221e83fcbdc968c8581161f314411776de5be93
Pawan <paletipawankumar@gmail.com>
2024-02-07 23:43:40 +0530

    modified the code

--- src/com/Pawan/Linear_Search/Minimum.java
+++ src/com/Pawan/Linear_Search/Minimum.java
@@ -5,6 +5,7 @@ public class Minimum {
         int[] nums = { 25 , 45 , 78 , 98 , 74 , 12 , -3 , 14 , 1 };
         System.out.println(min(nums));
     }
+
     static int min(int[] nums){
         int minimum = nums[0];
         for (int i = 1; i <nums.length ; i++) {
